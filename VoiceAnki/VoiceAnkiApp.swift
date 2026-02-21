db30ecdc975d64c2c452bdf61c49dee728e07b7f
Morgan Griffiths <morgangriffiths@openai.com>
2026-02-20 16:47:12 -0800

    Initial Commit

---
+++ VoiceAnki/VoiceAnkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceAnkiApp.swift
+//  VoiceAnki
+//
+//  Created by Morgan Griffiths on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct VoiceAnkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
